f8a8ab362554c5607e30122d75c3f0ae8d11b227
Jake Hernandez <jakejhdz@gmail.com>
2024-07-16 15:15:10 -0700

    Add http and node port parsing to commandline

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"os"
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 type Config struct {
-	ipAddr string
-	port   string
-	name   string
+	ipAddr   string
+	nodePort int
+	httpPort int
+	name     string
 }
 
 func main() {
@@ -21,9 +23,12 @@ func main() {
 	log.Printf(`Creating new node "%v" at %v`, config.name, config.FullAddr())
 	node := &Node{
 		Name: config.name, 
-		Addr: config.FullAddr(),
+		Addr: config.ipAddr,
+		Port: config.nodePort,
 	}
 	store := NewKVStore()
+
+	// Starts listeners to allow other nodes to join this memberlist.
 	cluster, err := NewCluster(node, store)
 
 	if err != nil {
@@ -32,26 +37,35 @@ func main() {
 	
 	api := NewAPI(cluster)
 
-	api.Run(":8080")
+	api.Run(fmt.Sprintf(":%v", config.httpPort))
 }
 
 
 func parseParams(params []string) (*Config, error){
-	if len(params) != 4 {
+	if len(params) != 5 {
 		return nil, errors.New(
-			"Invalid number of params; Enter 3 [name] [IP] [Port]",
+			"Invalid number of params; Enter 3 [name] [IP] [Node Port] [HTTP Port]",
 			)
 	}
-	name := params[1]
-	ipAddr := params[2]
-	port := params[3]
+	name     := params[1]
+	ipAddr   := params[2]
+	nodePort, err := strconv.Atoi(params[3])
+	if err != nil {
+		log.Fatalf("Error getting node port: %v", err)
+	}
+	httpPort, err := strconv.Atoi(params[4])
+	if err != nil {
+		log.Fatalf("Error getting http port: %v", err)
+	}
+
 	return &Config{
-		name: name,
-		port: port,
-		ipAddr: ipAddr,
+		name:     name,
+		nodePort: nodePort,
+		httpPort: httpPort,
+		ipAddr:   ipAddr,
 	}, nil
 }
 
 func (c *Config) FullAddr() string {
-	return fmt.Sprintf("%v:%v", c.ipAddr, c.port)
+	return fmt.Sprintf("%v:%v", c.ipAddr, c.nodePort)
 }
