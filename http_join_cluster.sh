d09169fbd9a595f8125134a9d5223b3888e5c44c
Jake Hernandez <jakejhdz@gmail.com>
2024-07-16 23:04:37 -0700

    Add docker container name instead of localhost

--- http_join_cluster.sh
+++ http_join_cluster.sh
@@ -3,5 +3,5 @@
 
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"ipAddress":"localhost:1235"}' \
+  --data '{"ipAddress":"node2:1235"}' \
   http://localhost:8080/join
