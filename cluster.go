b7dced06d4f0f60338793925ab3603754e39fa91
Jake Hernandez <jakejhdz@gmail.com>
2024-07-19 23:42:20 -0700

    Add add cluster delegate to begin sending data between nodes

--- cluster.go
+++ cluster.go
@@ -16,6 +16,8 @@ type Cluster struct {
 	store 	  *KVStore 
 }
 
+type ClusterDelegate struct{}
+
 func NewCluster(localNode *Node, store *KVStore, logger *log.Logger) (*Cluster, error) {
 	config := memberlist.DefaultLocalConfig()
 	config.Name = localNode.Name
@@ -27,18 +29,49 @@ func NewCluster(localNode *Node, store *KVStore, logger *log.Logger) (*Cluster, 
 		return nil, err
 	}
 
-	return &Cluster{
+	cluster := Cluster{
 		Memberlist: list,
 		LocalNode:  localNode,
 		store:		  store,
-	}, nil
+	}
+	config.Delegate = cluster
+	
+	return &cluster, nil
 }
 
-func (c *Cluster) Join(seeds []string) error {
+func (c Cluster) Join(seeds []string) error {
 	_, err := c.Memberlist.Join(seeds)
 	return err
 }
 
-func (c *Cluster) NotifyMsg(msg []byte) {
-
+func (c Cluster) NodeMeta(limit int) []byte{
+	return []byte{}
 }
+
+func (c Cluster) NotifyMsg(b []byte) {
+	log.Println("Notify Message")
+	log.Printf("%v\n", b)
+}
+
+func (c Cluster) GetBroadcasts(overhead, limit int) [][]byte {
+	return [][]byte{}
+}
+
+func (c Cluster) MergeRemoteState(buf []byte, join bool) {
+	// Recieves data from remote nodes
+	log.Printf("Merge remote state \n")
+	log.Printf("%v \n", buf)
+}
+
+func (c Cluster) LocalState(join bool) []byte {
+	// Sends local state to remote nodes:
+	log.Printf("Sending local data ")
+	val, ok := c.store.Get("test")
+	if ok {
+		return []byte(val)
+	} else {
+		return []byte{}
+	}
+}
+
+
