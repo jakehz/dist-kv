36a817c7a5444ad95ac99c33fe2ceab7d7faa5c6
Jake Hernandez <jakejhdz@gmail.com>
2024-08-01 00:49:12 -0700

    Add test for getting node based on key

--- hash_ring_test.go
+++ hash_ring_test.go
@@ -110,3 +110,65 @@ func TestPlaceNodeZero(t *testing.T) {
 		t.Fatalf("Node placed in hash ring of size zero. %v", hashRing.nodes)
 	}
 }
+
+func TestGetNodeTwo(t *testing.T) {
+	/*Case: Test where two nodes map to the same index*/
+	const RING_SIZE = 2
+	hashRing := NewHashRing(RING_SIZE)
+	// Create a single node
+	// Node should always map to index 1
+	node := NewNode("Node1")
+	hashRing.PlaceNode(&node)
+	node2 := NewNode("Node2")
+	// Create another node that maps to the same idx
+	hashRing.PlaceNode(&node2)
+	key1 := "key1"
+	key2 := "key2"
+	resNode1 := hashRing.GetNode(key1)
+	if resNode1.FullAddress().Addr != node2.FullAddress().Addr{ 
+		t.Fatalf(`Node returned for key %v is not node expected: Expected:"%v" actual:"%v"`, key1, node2.FullAddress().Addr, resNode1.FullAddress().Addr )
+	}
+	resNode2 := hashRing.GetNode(key2)
+	if resNode2.FullAddress().Addr != node.FullAddress().Addr{ 
+		t.Fatalf(`Node returned for key %v is not node expected: Expected:"%v" actual:"%v"`, key1, node.FullAddress().Addr, resNode2.FullAddress().Addr )
+	}
+}
+
+/*
+func TestPlaceNodeOne(t *testing.T) {
+	//Case: Test where two nodes map to the same index
+	const RING_SIZE = 1
+	// Create a single node
+	hashRing := NewHashRing(RING_SIZE)
+	node := NewNode("Node1")
+	node1Idx :=  hashRing.HashIdx(hashRing.NodeId(&node))
+	hashRing.PlaceNode(&node)
+	if hashRing.nodes[node1Idx] == nil && hashRing.nodes[node1Idx].Name == "Node1" {
+		t.Fatalf("Failed to place node %v in hash ring.", node)
+	}
+	// Create another node
+	node2 := NewNode("Node2")
+	node2Idx :=  hashRing.HashIdx(hashRing.NodeId(&node2))
+	if node2Idx != node1Idx{
+		t.Fatalf("Node2 idx should map Node1 idx")
+	}
+	hashRing.PlaceNode(&node2)
+	if hashRing.nodes[node2Idx] == nil && hashRing.nodes[node2Idx].Name =="Node2"{
+		t.Fatalf("Failed to place node %v in hash ring.", node2)
+	}
+}
+
+func TestPlaceNodeZero(t *testing.T) {
+	// Ensure that placing a node in an empty ring does not result in error
+	// or a non-empty ring
+	const RING_SIZE = 0
+	// Create a single node
+	hashRing := NewHashRing(RING_SIZE)
+	node := NewNode("Node1")
+	// node1Idx :=  hashRing.HashIdx(hashRing.NodeId(&node))
+	hashRing.PlaceNode(&node)
+	if len(hashRing.nodes) != 0{
+		t.Fatalf("Node placed in hash ring of size zero. %v", hashRing.nodes)
+	}
+}
+*/
